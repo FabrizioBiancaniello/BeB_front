05cace1e9cdf497f928b9c07755ff00585674cf1
Fabrizio Biancaniello <fabrizio.biancaniello@aulab.it>
2024-10-05 17:38:11 +0200

    Terminata pagina stanze e fixati alcuni problemi di responsività

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const navbar = document.querySelector(".navbar");
-const navbarBrand = document.querySelector(".navbar-brand");
-const navlinks = document.querySelectorAll(".nav-link");
+const navlinks = document.querySelectorAll(".nav-link-cus");
+const navbarTogglerCus = document.querySelector(".navbar-toggler-cus");
 
 window.addEventListener("scroll", ()=>{
     if(window.scrollY > 0){
         navbar.classList.add("nav-scrolled");
-        navbarBrand.classList.add("color-p");
-        navbarBrand.classList.remove("color-s");
+        navbarTogglerCus.classList.add("color-p")
+        navbarTogglerCus.classList.remove("color-s")
         navlinks.forEach((nl)=>{
             nl.classList.remove("color-s");
             nl.classList.add("color-p");
         })
     } else {
         navbar.classList.remove("nav-scrolled");
-        navbarBrand.classList.remove("color-p");
-        navbarBrand.classList.add("color-s");
+        navbarTogglerCus.classList.remove("color-p")
+        navbarTogglerCus.classList.add("color-s")
         navlinks.forEach((nl)=>{
             nl.classList.add("color-s");
             nl.classList.remove("color-p");
